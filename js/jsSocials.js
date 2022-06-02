f8d2fea853cb274e8315aa433ca1ef37f16e69c1
denilson53 <95344770+denilson53@users.noreply.github.com>
2022-06-02 19:35:55 -0300

    Add files via upload

---
+++ js/jsSocials.js
@@ -0,0 +1,6 @@
+$("#share").jsSocials({    
+    shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "whatsapp"]
+});
+
+$("#share").css('background-color','aquamarine').css('padding-left','30px');    
+  
